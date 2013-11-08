9f95189873493f61acd031b4126b62cc67d1df4c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-08 17:52:03 +0000

    copy setup files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.predictionio/trunk@26839 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+
+VERSION = '0.0.0'
+
+entry_points = {
+}
+
+TESTS_REQUIRE = []
+
+setup(
+	name='nti.predictionio',
+	version='0.0',
+	keywords='Experimental Code',
+	author='NTI',
+	author_email='jason.madden@nextthought.com',
+	description='NextThought Clients',
+	classifiers=[
+		"Development Status :: 4 - Alpha",
+		"Intended Audience :: Developers :: Education",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 2.7",
+		"Internet :: WWW/HTTP",
+		"Natural Language :: English"
+		],
+
+	tests_require=TESTS_REQUIRE, # Needed for e.g., tox
+	install_requires=[
+		'setuptools',
+		'predictionio >= 0.6.2'
+	],
+	extras_require={
+		'test': TESTS_REQUIRE,
+		'tools': [ ]
+	},
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	include_package_data=True,
+	namespace_packages=['nti'],
+	zip_safe=False,
+	entry_points=entry_points
+	)
