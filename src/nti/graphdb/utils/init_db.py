da83fe96672fc570a27f290ca23b4a5fdca93a51
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-13 05:00:32 +0000

    allow for simple user search
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@27028 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/graphdb/utils/init_db.py
+++ src/nti/graphdb/utils/init_db.py
@@ -18,22 +18,32 @@ from pyramid.view import view_config
 import pyramid.httpexceptions as hexc
 
 from nti.dataserver import authorization as nauth
+from nti.dataserver import interfaces as nti_interfaces
 
-from nti.graphdb import modeled
-from nti.graphdb import ratings
-from nti.graphdb import assessments
-from nti.graphdb import connections
-from nti.graphdb import discussions
+from nti import graphdb
 from nti.graphdb import interfaces as graph_interfaces
 
 from nti.utils.maps import CaseInsensitiveDict
 
+def _make_min_max_btree_range(search_term):
+	min_inclusive = search_term # start here
+	max_exclusive = search_term[0:-1] + unichr(ord(search_term[-1]) + 1)
+	return min_inclusive, max_exclusive
+
+def _username_search(search_term):
+	min_inclusive, max_exclusive = _make_min_max_btree_range(search_term)
+	dataserver = component.getUtility(nti_interfaces.IDataserver)
+	_users = nti_interfaces.IShardLayout(dataserver).users_folder
+	usernames = list(_users.iterkeys(min_inclusive, max_exclusive, excludemax=True))
+	return usernames
+		
 def init_db(db, usernames=()):
-	modeled.install(db, usernames)
-	ratings.install(db, usernames)
-	discussions.install(db, usernames)
-	connections.install(db, usernames)
-	assessments.install(db, usernames)
+	for module in (graphdb.modeled,
+				   graphdb.ratings,
+				   graphdb.discussions,
+				   graphdb.connections,
+				   graphdb.assessments):
+		module.install(db, usernames)
 
 @view_config(route_name='objects.generic.traversal',
 			 name='init_graphdb',
@@ -41,10 +51,13 @@ def init_db(db, usernames=()):
 			 permission=nauth.ACT_MODERATE)
 def init_graphdb(request):
 	values = json.loads(unicode(request.body, request.charset)) if request.body else {}
-	values = CaseInsensitiveDict(**values)
+	values = CaseInsensitiveDict(values)
 	site = values.get('site', u'')
+	term = values.get('term', values.get('search', None))
 	usernames = values.get('usernames', values.get('username', u''))
-	if isinstance(usernames,six.string_types):
+	if term:
+		usernames = _username_search(term)
+	elif isinstance(usernames, six.string_types):
 		usernames = usernames.split()
 	db = component.getUtility(graph_interfaces.IGraphDB, name=site)
 	init_db(db, usernames)
