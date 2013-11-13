17b0f96667e2f9602c1de52f01455b9f1806406f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-13 21:58:49 +0000

    fix import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.graphdb/trunk@27063 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/graphdb/dbfactory.py
+++ src/nti/graphdb/dbfactory.py
@@ -8,12 +8,14 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from . import _neo4j
+logger = __import__('logging').getLogger(__name__)
+
+from . import neo4j
 from . import interfaces as graph_interfaces
 
 def create_database(url, username=None, password=None, dbtype=graph_interfaces.NEO4J):
 	if dbtype.lower() == graph_interfaces.NEO4J:
-		result = _neo4j.Neo4jDB(url, username, password)
+		result = neo4j.Neo4jDB(url, username, password)
 	else:
 		raise Exception("Invalid database type")
 	return result
