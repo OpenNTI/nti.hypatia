29a7590443b36a3165ea3f104088a7d7cbb18d74
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-25 22:22:16 +0000

    remove unused
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@27391 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/__init__.py
+++ src/nti/hypatia/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-graphdb module
+hypatia module
 
 $Id$
 """
@@ -10,31 +10,3 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import six
-
-from zope import component
-from zope.component.interfaces import ComponentLookupError
-
-from pyramid.threadlocal import get_current_request
-
-from . import interfaces as gdb_interfaces
-
-def get_possible_site_names(request=None, include_default=True):
-    request = request or get_current_request()
-    if not request:
-        return () if not include_default else ('',)
-    __traceback_info__ = request
-
-    site_names = getattr(request, 'possible_site_names', ())
-    if include_default:
-        site_names += ('',)
-    return site_names
-
-def get_graph_db(names=None, request=None):
-    names = names.split() if isinstance(names, six.string_types) else names
-    names = names or get_possible_site_names(request=request)
-    for site in names:
-        app = component.queryUtility(gdb_interfaces.IGraphDB, name=site)
-        if app is not None:
-            return app
-    return None
