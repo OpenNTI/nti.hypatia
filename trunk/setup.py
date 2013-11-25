4345a009e49375fea9457417874b3d8d70b6fc42
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-25 16:13:57 +0000

    copy
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@27370 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ trunk/setup.py
@@ -0,0 +1,35 @@
+import codecs
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+
+entry_points = {
+}
+
+setup(
+    name='nti.graphdb',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI graphdb",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='pyramid preference',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        ],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti'],
+	install_requires=[
+		'setuptools',
+        'py2neo'
+	],
+	entry_points=entry_points
+)
