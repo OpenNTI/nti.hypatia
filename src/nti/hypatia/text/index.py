97da27d703c4ef1b5e331e1221c1209044f91301
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-05 01:35:11 +0000

    in case we need add classes to store the source text that is indexed
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@28919 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/hypatia/text/index.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from hypatia.text.okapiindex import OkapiIndex
+from hypatia.text.cosineindex import CosineIndex
+
+class SourceBaseIndexMixin(object):
+
+	def reset(self):
+		super(SourceBaseIndexMixin, self).reset()
+		self._textsource = self.family.IO.BTree()
+
+	def index_doc(self, docid, text):
+		super(SourceBaseIndexMixin, self).index_doc(docid, text)
+		self._textsource[docid] = text or u''
+
+	def reindex_doc(self, docid, text):
+		super(SourceBaseIndexMixin, self).reindex_doc(docid, text)
+		self._textsource[docid] = text or u''
+
+	def unindex_doc(self, docid):
+		super(SourceBaseIndexMixin, self).unindex_doc(docid)
+		if docid in self._textsource: 
+			del self._textsource[docid]
+
+class SourceCosineIndex(SourceBaseIndexMixin, CosineIndex):
+
+	def __init__(self, *args, **kwargs):
+		CosineIndex.__init__(self, *args, **kwargs)
+
+class SourceOkapiIndex(SourceBaseIndexMixin, OkapiIndex):
+
+	def __init__(self, *args, **kwargs):
+		OkapiIndex.__init__(self, *args, **kwargs)
