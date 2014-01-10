76b9807539a0f858b0977250488463bbf2ffa481
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-10 20:35:18 +0000

    up the queue size
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@29841 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/generations/install.py
+++ src/nti/hypatia/generations/install.py
@@ -63,7 +63,7 @@ def install_queue(context):
 	lsm = dataserver_folder.getSiteManager()
 	intids = lsm.getUtility(zope.intid.IIntIds)
 
-	queue = CatalogQueue(83)
+	queue = CatalogQueue(137)
 	queue.__parent__ = dataserver_folder
 	queue.__name__ = '++etc++hypatia++catalogqueue'
 	intids.register(queue)
