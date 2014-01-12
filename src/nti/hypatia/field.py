dda8a5479cf2c715db854bd8fb453c726538ede5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-01-12 08:44:52 +0000

    spelling
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@30078 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/field.py
+++ src/nti/hypatia/field.py
@@ -24,20 +24,17 @@ from .interfaces import ITimeFieldIndex
 class TimeFieldIndex(FieldIndex):
 
 	def reset(self):
-		"""
-		Initialize forward and reverse mappings.
-		"""
+		self._num_docs = Length(0)
 		# The forward index maps indexed values to a sequence of docids
 		self._fwd_index = self.family.IO.BTree()
 		# The reverse index maps a docid to its index value
 		# use a II tree since we are storing time as ints
 		self._rev_index = self.family.II.LLBTree()
-		self._num_docs = Length(0)
 		self._not_indexed = self.family.II.TreeSet()
 
 	def discriminate(self, obj, default):
 		value = super(TimeFieldIndex, self).discriminate(obj, default)
 		assert type(value) in (float, int)
-		if type(value) == float:  # auto-covert
+		if type(value) == float:  # auto-convert
 			value = time_to_64bit_int(value)
 		return value
