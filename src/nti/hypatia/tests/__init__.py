4294780b3069335a21d1b51749739765ccc1b39c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-01 17:03:48 +0000

    Tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@33968 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/tests/__init__.py
+++ src/nti/hypatia/tests/__init__.py
@@ -68,10 +68,15 @@ class SharedConfiguringTestLayer(ZopeComponentLayer,
     @classmethod
     def tearDown(cls):
         cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
 
     @classmethod
     def testSetUp(cls, test=None):
         cls.setUpTestDS(test)
+        
+    @classmethod
+    def testTearDown(cls):
+        pass
 
 class HypatiaApplicationTestLayer(ApplicationTestLayer):
 
