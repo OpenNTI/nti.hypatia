5a2732a78dbd303f0f4db3511ece6bfcee467894
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-17 17:09:16 +0000

    add test and small migration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@53912 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/generations/install.py
+++ src/nti/hypatia/generations/install.py
@@ -10,7 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 7
+generation = 8
 
 from zope.generations.generations import SchemaManager
 
