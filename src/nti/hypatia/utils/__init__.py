f0103a8421806362534fd578811c040ec09e73d5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-21 22:30:27 +0000

    add resolve param
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@54397 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/utils/__init__.py
+++ src/nti/hypatia/utils/__init__.py
@@ -24,32 +24,31 @@ from nti.externalization.oids import to_external_oid
 
 from nti.hypatia import is_indexable
 
-def _is_indexable_and_valid_object(obj, usernames=()):
+def _is_indexable_and_valid_object(obj, usernames=(), resolve=True):
 	# get the object creator to try to trigger a POSError 
 	# if the object is invalid
 	creator = getattr(obj, 'creator', None) or u''
 	username = getattr(creator, 'username', creator).lower()
 	result =  is_indexable(obj) and not IDeletedObjectPlaceholder.providedBy(obj) and \
 			  (not usernames or username in usernames) 
-	if result:
+	if result and resolve:
 		# resolve the content to try trigger POSError if the object is invalid
-		resolver = IContentResolver(obj, None)
-		getattr(resolver, 'content', None)
+		IContentResolver(obj).content
 	return result
 		
-def all_indexable_objects_iids(users=()):
+def all_indexable_objects_iids(users=(), resolve=True):
 	obj = None
 	intids = component.getUtility(zope.intid.IIntIds)
 	usernames = {getattr(user, 'username', user).lower() for user in users or ()}
 	for uid in intids:
 		try:
 			obj = intids.getObject(uid)
-			if _is_indexable_and_valid_object(obj, usernames):
+			if _is_indexable_and_valid_object(obj, usernames, resolve=resolve):
 				yield uid, obj
-		except (POSError, TypeError) as e:
+		except (POSError, TypeError, AttributeError) as e:
 			logger.error("Ignoring %s(%s); %s", type(obj), uid, e)
 
-def all_cataloged_objects(users=()):
+def all_cataloged_objects(users=(), resolve=True):
 	obj = None
 	intids = component.getUtility(zope.intid.IIntIds)
 	catalog = component.getUtility(ICatalog, METADATA_CATALOG_NAME)
@@ -62,7 +61,7 @@ def all_cataloged_objects(users=()):
 	for uid in intids_created_by:
 		try:
 			obj = intids.getObject(uid)
-			if 	_is_indexable_and_valid_object(obj):
+			if 	_is_indexable_and_valid_object(obj, resolve=resolve):
 				yield uid, obj
-		except (POSError, TypeError) as e:
+		except (POSError, TypeError, AttributeError) as e:
 			logger.error("Ignoring %s(%s); %s", type(obj), uid, e)
