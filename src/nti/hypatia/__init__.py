4aadb477890fb566c5c314fbf72354de78b4d17a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-11-24 15:15:47 +0000

    enable migration
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@54477 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/__init__.py
+++ src/nti/hypatia/__init__.py
@@ -3,11 +3,13 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import time
 import itertools
 
 from zope import component
@@ -69,8 +71,9 @@ def process_queue(queue=None, limit=DEFAULT_QUEUE_LIMIT, sync_queue=True,
 	limit = queue_size if limit == -1 else limit
 	to_process = min(limit, queue_size)
 	if queue_size > 0:
-		logger.info("Taking %s event(s) to process; current queue size %s",
-					to_process, queue_size)
-		queue.process(ids, (catalog,), to_process, ignore_pke=ignore_pke)
-
+		now = time.time()
+		logger.debug("Taking %s event(s) to process; current queue size %s",
+					 to_process, queue_size)
+		done = queue.process(ids, (catalog,), to_process, ignore_pke=ignore_pke)
+		logger.debug("%s event(s) processed in %s(s)", done, time.time()-now)
 	return to_process
