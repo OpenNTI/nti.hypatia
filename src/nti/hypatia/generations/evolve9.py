b8be03678d80cb6b8c422699198472b489d1eff7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-25 19:20:15 +0000

    place holder to remove hypatia objects
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@99352 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/hypatia/generations/evolve9.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 9
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import setHooks
+from zope.component.hooks import site as current_site
+
+from zope.intid.interfaces import IIntIds
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid(self, oid, ignore_creator=False):
+		resolver = component.queryUtility(IOIDResolver)
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+		return None
+
+def _remove_hypatia(lsm, intids):
+	try:
+		from nti.hypatia.interfaces import ISearchCatalog
+		from nti.hypatia.interfaces import ISearchCatalogQueue
+		
+		for provided in (ISearchCatalog, ISearchCatalogQueue):
+			obj = lsm.queryUtility(provided=provided)
+			if obj is not None:
+				intids.unregister(obj)
+				lsm.unregisterUtility(obj, provided=provided)
+				obj.__parent__ = None
+	except ImportError:
+		pass
+
+def do_evolve(context, generation=generation):
+	logger.info("Evolution %s started", generation);
+
+	setHooks()
+	conn = context.connection
+	ds_folder = conn.root()['nti.dataserver']
+	
+	mock_ds = MockDataserver()
+	mock_ds.root = ds_folder
+	component.provideUtility(mock_ds, IDataserver)
+
+	with current_site(ds_folder):
+		assert 	component.getSiteManager() == ds_folder.getSiteManager(), \
+				"Hooks not installed?"
+
+		lsm = ds_folder.getSiteManager()
+		intids = lsm.getUtility(IIntIds)
+		_remove_hypatia(lsm, intids)
+
+	component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+	logger.info('Evolution %s done.', generation)
+
+def evolve(context):
+	"""
+	Evolve to generation 9 to remove hypatia registered objects.
+	"""
+	do_evolve(context)
