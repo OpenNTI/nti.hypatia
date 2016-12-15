bb78b2095a8568ffe0fa2c1ec971c45e31e2ac78
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-15 19:51:24 +0000

    enable generation 9 to remove hypatia objects
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.hypatia/trunk@102695 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/hypatia/generations/install.py
+++ src/nti/hypatia/generations/install.py
@@ -11,7 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-generation = 8
+generation = 9
 
 from zope.generations.generations import SchemaManager
 
